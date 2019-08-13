76d689a1ee9e2ec2b9e2747c31e85e1f96c8df0f
Nihey Takizawa <nihey.takizawa@gmail.com>
2019-08-13 15:43:56 -0300

    Add POST API

--- index.js
+++ index.js
@@ -1,30 +1,71 @@
 const SheetBest = {};
 
-SheetBest.input = () => {
-  // TODO
-  // const url = element.getAttribute('data-sheet-best');
+SheetBest.input = (form) => {
+  // XXX: Not yet tested
+  const url = form.getAttribute('data-sheet-best');
+
+  const setEnabled = (enabled) => {
+    const elements = form.querySelectorAll('[type=submit]');
+    for (let i = 0; i < elements.length; i++) {
+      const element = elements[i];
+      if (enabled) {
+        element.removeAttribute('disabled');
+      } else {
+        element.setAttribute('disabled', 'disabled');
+      }
+    }
+  };
+
+
+  form.addEventListener('submit', async (e) => {
+    setEnabled(false);
+
+    e.preventDefault();
+    const formData = new FormData(form);
+    const entries = Array.from(formData);
+    const data = {};
+    entries.forEach(([k, v]) => {
+      data[k] = v;
+    });
+
+    form.querySelectorAll('[type=submit]');
+
+    const response = await fetch(url, {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify([data]),
+    }).then((r) => r.json());
+
+    form.dispatchEvent(new Event('submit-finished', {
+      response,
+    }));
+
+    setEnabled(true);
+  });
 };
 
-SheetBest.output = (element) => {
+SheetBest.output = async (element) => {
   // Extract some data, from the element
   const url = element.getAttribute('data-sheet-best');
   const text = element.innerHTML;
   element.innerHTML = '';
 
-  fetch(url).then((r) => r.json()).then((data) => {
-    const replacement = data.map((object) => {
-      // Trim all attributes on the object
-      Object.keys(object).forEach((k) => {
-        object[k.trim()] = object[k];
-      });
-
-      // Replace the text
-      return text.replace(/{{([^{}]*)}}/g, (match, key) => object[key.trim()]);
+  const data = await fetch(url).then((r) => r.json());
+  const replacement = data.map((object) => {
+    // Trim all attributes on the object
+    Object.keys(object).forEach((k) => {
+      object[k.trim()] = object[k];
     });
 
-    // Re-add it to the HTML
-    element.innerHTML = replacement.join('');
+    // Replace the text
+    return text.replace(/{{([^{}]*)}}/g, (match, key) => object[key.trim()]);
   });
+
+  // Re-add it to the HTML
+  element.innerHTML = replacement.join('');
 };
 
 SheetBest.setup = () => {
